88c77a6b606fcc110bf7b0b0d5078e1c35b8c150
pelkoa <hvngmvnvibe@gmail.com>
2020-11-22 23:39:05 +0300

    python class problems

---
+++ User(9).py
@@ -0,0 +1,24 @@
+class User:
+    def __init__(self, first_name, last_name, age, height, weight):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.height = height
+        self.weight = weight
+
+    def describe_user(self):
+        print(f"User's name is {self.first_name} {self.last_name}.")
+        print(f"User's age is {self.age}.")
+        print(f"User's height is {self.height}.")
+        print(f"User's weight is {self.weight}.")
+
+    def greet_user(self):
+        print(f"Hello {self.first_name} {self.last_name}")
+
+
+user = User('Sereja', 'Koroteev', 26, 182, 80)
+user_ka = User('Olya', 'Abramova', 25, 174, 64)
+user.describe_user()
+user.greet_user()
+user_ka.describe_user()
+user_ka.greet_user()
---
+++ restaurant.py
@@ -0,0 +1,22 @@
+class Restaurant:
+
+    def __init__(self, restaurant_name, cuisine_type):
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+        self.number_served = 0
+
+    def describe_restaurant(self):
+        print(f"Restaurant name is {self.restaurant_name}.")
+        print(f"Cuisine type is {self.cuisine_type}.")
+
+    def open_restaurant(self):
+        print(f"{self.restaurant_name} now is open.")
+
+    def served(self):
+        print(f"Number of visitors served is {self.number_served}.")
+
+restaurant_0 = Restaurant('KFC', 'Backed chicken')
+restaurant_1 = Restaurant('Mac', 'Beef')
+restaurant_2 = Restaurant('Subway', 'Sandwich')
+
+restaurant_0.served()
