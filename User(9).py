560faf93f046be7b6e70a195366f4c04c161a44b
pelkoa <hvngmvnvibe@gmail.com>
2020-11-24 23:44:33 +0300

    добавлены методы increment_login attempts, reset_login_attempts, show_login_attempts  и их проверка

--- User(9).py
+++ User(9).py
@@ -1,10 +1,20 @@
 class User:
-    def __init__(self, first_name, last_name, age, height, weight):
+    def __init__(self, first_name, last_name, age, height, weight, login_attempts=0):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
         self.height = height
         self.weight = weight
+        self.login_attempts = login_attempts
+
+    def increment_login_attempts(self):
+        self.login_attempts += 1
+
+    def reset_login_attempts(self):
+        self.login_attempts = 0
+
+    def show_login_attempts(self):
+        print(f"Number of login attempts = {self.login_attempts}")
 
     def describe_user(self):
         print(f"User's name is {self.first_name} {self.last_name}.")
@@ -18,7 +28,10 @@ class User:
 
 user = User('albert', 'einstein', 26, 182, 80)
 user_ka = User('mary', 'kury', 25, 174, 64)
-user.describe_user()
-user.greet_user()
-user_ka.describe_user()
-user_ka.greet_user()
+
+user.increment_login_attempts()
+user.increment_login_attempts()
+user.increment_login_attempts()
+user.show_login_attempts()
+user.reset_login_attempts()
+user.show_login_attempts()
